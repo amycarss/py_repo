56e91b153b21914be6e756341ab3316a289b24cf
amycarss <amycarss@umich.edu>
2018-04-29 15:42:04 -0400

    Update car.py with new request code
    Change request code from what we used in class to an example provided by the NHTSA site

--- car.py
+++ car.py
@@ -1,28 +1,14 @@
-
+#get example from NHTSA
 import requests
 import json
-NYT_KEY ="9a9e735727d34d8687de1862420929bf"
-# What's the base url?
+url = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=json'
+r = requests.get(url)
+print(r.text)	
 
-# Build your parameters dictionary.
-baseurl="https://api.nytimes.com/svc/search/v2/articlesearch.json"
-params_d={}
-params_d['api-key'] =NYT_KEY
-params_d["q"]='query'
-#can compare to code examples to see how to handle the way album is nested beneath entity, can do some trial and error, stick in in a borwser and see if it outputs anything
-# Input the parameters to the get function in requests
-resp_obj=requests.get(baseurl, params=params_d)#response object
-# Access the text attribute of the response object
-text_str=resp_obj.text
-# Load the string into a Python list or dictionary
-py_diction=json.loads(text_str)#walked through this much in class, what's below may not be correct
-## Part 2:
-# Dump the Python object to a JSON-formatted string
-jsn_str=json.dumps(py_diction)
-# Open a file for writing
-f=open('monae_data.json','w')
-# Write the JSON-formatted string to the file
-f.write(jsn_str)
-f.close()
-# Close the file
-print(py_diction)
+#post example
+import requests
+import json
+url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/'
+post_fields = {'format': 'json', 'data':'3GNDA13D76S000000;5XYKT3A12CG000000'}
+r = requests.post(url, data=post_fields)
+print(r.text)		
